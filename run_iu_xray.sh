0155f0bc40c4d3a7e1b396dc9c306b6b99a0ad40
Matheus Xavier Sampaio <matheus.sampaio011@gmail.com>
2022-05-29 22:22:11 -0300

    Update run_iu_xray.sh

--- run_iu_xray.sh
+++ run_iu_xray.sh
@@ -5,9 +5,9 @@ python main.py \
     --max_seq_length 60 \
     --threshold 3 \
     --batch_size 32 \
-    --epochs 50 \
+    --epochs 70 \
     --save_dir results/iu_xray \
-    --step_size 70 \
+    --step_size 50 \
     --gamma 0.1 \
     --seed 9223 \
     --visual_extractor resnet \
